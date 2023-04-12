9d479b7d7a4d5223a65a7a76b566c8ccc06ee698
re11n <97107417+re11n@users.noreply.github.com>
2023-04-12 17:03:22 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302200003/BankTransferConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace modul8_1302200003
+{
+    class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public transfers transfer { get; set; }
+        public string method { get; set; }
+        public confirmation confirmation { get; set; }
+        public BankTransferConfig() { }
+        public BankTransferConfig(string lang, transfers transfer, string method, confirmation confirmation)
+        {
+            this.lang = lang;
+            this.transfer = transfer;
+            this.method = method;
+            this.confirmation = confirmation;
+        }
+
+        internal dynamic ReadJson()
+        {
+            string file = "bank_transfer_config.json";
+            string dataStr = File.ReadAllText(file);
+            BankTransferConfig dataBank = JsonSerializer.Deserialize<BankTransferConfig>(dataStr);
+            return dataBank;
+        }
+    }
+
+    class transfers
+    {
+        public string threshold { get; set; }
+        public string low_fee { get; set; }
+        public string high_fee { get; set; }
+        public transfers() { }
+        public transfers(string threshold, string low_fee, string high_fee)
+        {
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+        }
+    }
+
+    class confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+        public confirmation() { }
+        public confirmation(string en, string id)
+        {
+            this.en = en;
+            this.id = id;
+        }
+
+    }
+
+
+
+}
--- modul8_1302200003/Program.cs
+++ modul8_1302200003/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace modul8_1302200003
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            BankTransferConfig bkconfig = new BankTransferConfig();
+            dynamic config = GetConfig(bkconfig);
+
+            if (config.lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            }
+            else if (config.lang == "id")
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+            }
+
+            string uangDiTranferStr = Console.ReadLine();
+            int uangDiTransfer = int.Parse(uangDiTranferStr);
+            int biayaTransfer;
+
+            if (uangDiTransfer <= int.Parse(config.transfer.threshold))
+            {
+                biayaTransfer = int.Parse(config.transfer.low_fee);
+            } else
+            {
+                biayaTransfer = config.transfer.high_fee;
+            }
+
+            if (config.lang == "en")
+            {
+                Console.WriteLine("Transfer fee = " + biayaTransfer);
+                Console.WriteLine("Total amount = " + (biayaTransfer + uangDiTransfer) );
+
+            } else if ( config.lang == "id")
+            {
+                Console.WriteLine("Biaya Transfer = " + biayaTransfer);
+                Console.WriteLine("Total biaya = " + (biayaTransfer + uangDiTransfer));
+
+            }
+
+            string confirm;
+            if(config.lang == "en")
+            {
+                Console.WriteLine("Please type '" + config.confirmation.en + "' to confirm he transaction: ");
+                confirm = Console.ReadLine();
+
+                if (confirm == (string)config.confirmation.en)
+                {
+                    Console.WriteLine("the transfer is completed");
+                } else
+                {
+                    Console.WriteLine("transfer is canceled");
+                }
+            } else if (config.lang == "id")
+            {
+                Console.WriteLine("ketik '" + config.confirmation.en + "' untuk mengkonfirmasi transaksi: ");
+                confirm = Console.ReadLine();
+
+                if (confirm == (string)config.confirmation.en)
+                {
+                    Console.WriteLine("transfer berhasil");
+                }
+                else
+                {
+                    Console.WriteLine("transfer dibatalkan");
+                }
+            }
+
+        }
+
+        static dynamic GetConfig(BankTransferConfig bkconfig)
+        {
+            return bkconfig.ReadJson();
+        }
+
+
+    }
+}
